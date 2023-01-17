44a9f83528f57ce8e24ff96818fba59aabe8423c
Gamzegül Karahisarlı <gamzegulkarahisarli@gmail.com>
2023-01-17 21:23:53 +0300

    .

--- Projects/app.py
+++ Projects/app.py
@@ -127,7 +127,7 @@ import joblib
 # If button is pressed
 if st.button("Submit"):
     # Unpickle classifier
-    clf = joblib.load('DecisionTreeModel.pkl')
+    clf = joblib.load("Projects/DecisionTreeModel.pkl")
 
     # Get prediction
     prediction = clf.predict(res)
