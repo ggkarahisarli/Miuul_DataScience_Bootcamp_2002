d5ae50844e0dd1d726efb0e94a2f13dff737e4a8
Gamzegül Karahisarlı <gamzegulkarahisarli@gmail.com>
2023-01-17 22:16:37 +0300

    .

--- Projects/app.py
+++ Projects/app.py
@@ -128,7 +128,7 @@ import pickle
 #load model:
 
 
-pickle_in = open("DecisionTreeModel.pkl", 'rb')
+pickle_in = open("ggkarahisarli/Miuul_DataScience_and_MachineLearning_Bootcamp_2002/Projects/DecisionTreeModel.pkl", 'rb')
 clf = pickle.load(pickle_in)
 # If button is pressed
 if st.button("Submit"):
